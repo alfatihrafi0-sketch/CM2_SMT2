6baf073e1ab99c166bca61d85e407b0f90b8256c
alfatihrafi0-sketch <alfatihrafi0@gmail.com>
2026-05-22 08:40:24 +0700

    modif

--- PesananDll.java
+++ PesananDll.java
@@ -1,53 +1,85 @@
 public class PesananDll {
-    NodePesanan head;    // Pointer head digunakan untuk menunjuk node pertama pada linked list pesanan
-    NodePesanan tail;    // Pointer tail digunakan untuk menunjuk node terakhir pada linked list pesanan
+    NodePesanan head; // Pointer head digunakan untuk menunjuk node pertama pada linked list pesanan
+    NodePesanan tail; // Pointer tail digunakan untuk menunjuk node terakhir pada linked list pesanan
 
-    PesananDll() {  // Konstruktor inisialisasi Dll kosong
+    PesananDll() { // Konstruktor inisialisasi Dll kosong
         head = null;
         tail = null;
     }
-    
-     // Method untuk menambahkan pesanan baru
+
+    // Method untuk menambahkan pesanan baru
     void tambahPesanan(int kodePesanan, String namaPesanan, int harga) {
-        Pesanan p         = new Pesanan(kodePesanan, namaPesanan, harga); 
-        NodePesanan baru  = new NodePesanan(p);                          
+        Pesanan p = new Pesanan(kodePesanan, namaPesanan, harga);
+        NodePesanan baru = new NodePesanan(p);
 
         if (head == null) {
             head = baru;
             tail = baru;
         } else {
-            tail.next  = baru;
-            baru.prev  = tail;
-            tail       = baru;
+            tail.next = baru;
+            baru.prev = tail;
+            tail = baru;
         }
     }
 
-   // Method untuk menampilkan laporan pesanan
+    // Method untuk menampilkan laporan pesanan
     void laporanPesanan() {
         if (head == null) {
             System.out.println("Belum ada pesanan.");
             return;
         }
 
-        boolean swapped; // Proses sorting menggunakan Bubble Sort
-        do {
-            swapped = false;
-            NodePesanan current = head;   // Current digunakan untuk traversal
+        // boolean swapped; // Proses sorting menggunakan Bubble Sort
+        // do {
+        // swapped = false;
+        // NodePesanan current = head; // Current digunakan untuk traversal
 
-            while (current != null && current.next != null) {
+        // while (current != null && current.next != null) {
 
-                if (current.data.namaPesanan.compareToIgnoreCase(current.next.data.namaPesanan) > 0) {
+        // if
+        // (current.data.namaPesanan.compareToIgnoreCase(current.next.data.namaPesanan)
+        // > 0) {
 
-                    Pesanan tmp        = current.data;
-                    current.data       = current.next.data;
-                    current.next.data  = tmp;
+        // Pesanan tmp = current.data;
+        // current.data = current.next.data;
+        // current.next.data = tmp;
 
-                    swapped = true;  
-                }
-                current = current.next;
+        // swapped = true;
+        // }
+        // current = current.next;
+        // }
+        // } while (swapped); // Sorting berhenti jika tidak ada pertukaran lagi
+
+        // Modifikasi Menjadi insertion sort
+        NodePesanan i = head.next;
+
+        // Perulangan dimulai dari node kedua
+        while (i != null) {
+
+            // Menyimpan data yang akan dibandingkan
+            Pesanan key = i.data;
+
+            NodePesanan j = i.prev; // Pointer j digunakan untuk bergerak ke kiri
+
+            // Selama data sebelumnya lebih besar dari key
+            while (j != null &&
+                    j.data.namaPesanan.compareToIgnoreCase(key.namaPesanan) > 0) {
+
+                j.next.data = j.data; // Menggeser data sebelumnya ke kanan
+
+                j = j.prev; // Bergerak ke node sebelumnya
             }
-        } while (swapped);   // Sorting berhenti jika tidak ada pertukaran lagi
-       
+            if (j == null) { // jika j menjadi null berarti data ditempatkan di head
+
+                head.data = key;
+            } else {
+                // Menempatkan key pada posisi yang benar
+                j.next.data = key;
+            }
+            // Pindah ke node berikutnya
+            i = i.next;
+        }
+
         System.out.println("======================================");
         System.out.println("LAPORAN PESANAN (URUT NAMA PESANAN)");
         System.out.println("======================================");
@@ -57,10 +89,10 @@ public class PesananDll {
         NodePesanan current = head;
         while (current != null) {
             System.out.printf("%-14d %-20s %-10d%n",
-                current.data.kodePesanan,
-                current.data.namaPesanan,
-                current.data.harga);
-            total  += current.data.harga;
+                    current.data.kodePesanan,
+                    current.data.namaPesanan,
+                    current.data.harga);
+            total += current.data.harga;
             current = current.next;
         }
 
